fd85de3435539b9e1acf1eb5e232477fba09b86d
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-06-08 16:57:00 -0400

    Added Bullet Hit Effect On Walls

---
+++ Assets/_Scripts/HitEffects.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffects : MonoBehaviour, IHitable
+{
+    [SerializeField] GameObject effectsPrefabs;
+
+    private ParticleSystem effectsCache;
+
+    public void Hit(RaycastHit hit)
+    {
+        if (effectsCache != null)
+        {
+            effectsCache.transform.position = hit.point;
+            effectsCache.transform.rotation = Quaternion.LookRotation(hit.normal);
+            effectsCache.Play();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (effectsPrefabs != null)
+        {
+            GameObject effectsTemp = Instantiate(effectsPrefabs, transform);
+            effectsCache = effectsTemp.GetComponent<ParticleSystem>();
+
+
+        }
+    }
+
+}
