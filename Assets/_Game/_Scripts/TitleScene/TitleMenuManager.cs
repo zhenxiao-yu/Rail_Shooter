f16b40f3a2163af10f7fdb597b19cd0ea19eedfa
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-06-20 10:47:42 -0400

    Added Options Menu Script

--- Assets/_Game/_Scripts/TitleScene/TitleMenuManager.cs
+++ Assets/_Game/_Scripts/TitleScene/TitleMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleMenuManager : MonoBehaviour
 {
@@ -10,5 +11,39 @@ public class TitleMenuManager : MonoBehaviour
 
   [SerializeField] Button startButton, optionButton, optionCloseButton, quitButton;
   
+  void Start()
+  {
+    optionPanel.SetActive(false);
+    startButton.Select();
+
+    startButton.onClick.AddListener(StartGame);
+    optionButton.onClick.AddListener(OpenOptionPanel);
+    optionCloseButton.onClick.AddListener(CloseOptionPanel);
+    quitButton.onClick.AddListener(QuitGame);
+  }
+
+  void StartGame()
+  {
+    if (optionPanel.activeInHierarchy)
+        return;
+    
+    SceneManager.LoadScene(gamePlayScene);
+  }
+
+  void OpenOptionPanel()
+  {
+    optionPanel.SetActive(true);
+  }
+
+  void CloseOptionPanel()
+  {
+    optionPanel.SetActive(false);
+    optionButton.Select();
+  }
+
+  void QuitGame()
+  {
+    Application.Quit();
+  }
 
 }
